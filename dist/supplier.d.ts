479ebac6a23432797d1cfcae87a6a2212b665880
Moises Samuel Maison Maison <toinmdq@gmail.com>
2024-03-14 12:52:49 +0100

    add SupplierInvoice interface

--- dist/supplier.d.ts
+++ dist/supplier.d.ts
@@ -15,5 +15,23 @@ declare global {
         service: boolean;
         disabled: boolean;
     }
+    interface SupplierInvoice {
+        storeId: string;
+        invoiceId: string;
+        createdAt: number;
+        type: 'FAC' | 'ND' | 'NC';
+        dated: number;
+        number: string;
+        razonSocial: string;
+        cuit: number;
+        neto: number;
+        iva10: number;
+        iva21: number;
+        total: number;
+        per_iibb: number;
+        per_iva: number;
+        file: string;
+        currencyValue: number;
+    }
 }
 export {};
--- src/supplier.ts
+++ src/supplier.ts
@@ -20,6 +20,27 @@ declare global {
 		disabled: boolean;
 	}
 
+	interface SupplierInvoice {
+		storeId: string;
+		invoiceId: string;
+		createdAt: number;
+		// invoice data
+		type: 'FAC' | 'ND' | 'NC';
+		dated: number;
+		number: string;
+		razonSocial: string;
+		cuit: number;
+		// details
+		neto: number;
+		iva10: number;
+		iva21: number;
+		total: number;
+		per_iibb: number;
+		per_iva: number;
+		file: string;
+		currencyValue: number;
+	}
+
 }
 
 export { };
