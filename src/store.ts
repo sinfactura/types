eca493267e4354af9986fcbee2a487ffb2521f00
Moises Samuel Maison Maison <toinmdq@gmail.com>
2024-12-31 11:32:58 +0100

    add basic feature flags

--- src/store.ts
+++ src/store.ts
@@ -1,190 +1,240 @@
-
 declare global {
+  interface Config {
+    // PK // global config
+    appVersion: number;
+    fiscalConditions: FiscalCondition[];
+    ivaTypes: Method[];
+    minWithDni: number;
+    notificationOptions: {
+      id: string;
+      name: string;
+    }[];
+    stats: {
+      store: number;
+    };
+  }
 
-	interface Config { // PK // global config
-		appVersion: number;
-		fiscalConditions: FiscalCondition[];
-		ivaTypes: Method[];
-		minWithDni: number;
-		notificationOptions: {
-			id: string,
-			name: string;
-		}[];
-		stats: {
-			store: number;
-		};
-	}
+  interface Ecommerce {
+    isActive?: boolean;
+    config?: {
+      showDefaultPriceList?: number; // 0: NONE, 1: PUBLICO, 2: TECNICO, 3: GREMIO, 4: MAYORISTA
+      showStocks?: boolean;
+    };
+    sidebar?: {
+      show?: boolean;
+      categories?: boolean;
+      brands?: boolean;
+      incomes?: boolean;
+      favorites?: boolean;
+      basket?: boolean;
+      orders?: boolean;
+      invoices?: boolean;
+    };
+    footerBar?: {
+      show?: boolean;
+      orders?: boolean;
+      invoices?: boolean;
+      basket?: boolean;
+      favorites?: boolean;
+    };
+    themeColors?: {
+      main?: string;
+      navbar?: string;
+    };
+    appVersion?: number;
+    stats?: Record<string, string>;
+  }
 
-	interface Ecommerce {
-		isActive?: boolean;
-		config?: {
-			showPricesNoLogin?: boolean;
-			showStocks?: boolean;
-		};
-		sideBar?: {
-			show?: boolean;
-			showCategories?: boolean;
-			showBrands?: boolean;
-			showIncomes?: boolean;
-			showFavorites?: boolean;
-			showBasket?: boolean;
-			showOrders?: boolean;
-			showInvoices?: boolean;
-		};
-		footerBar?: {
-			show?: boolean;
-			showOrders?: boolean;
-			showInvoices?: boolean;
-			showBasket?: boolean;
-			showFavorites?: boolean;
-		}
-		themeColors?: {
-			main?: string;
-			navbar?: string;
-		};
-		appVersion?: number;
-		stats?: Record<string, string>;
-	}
+  interface FeatureFlags {
+    navbar?: {
+      show?: boolean;
+    };
+    sidebar?: {
+      show?: boolean;
+      customerFinder?: {
+        show?: boolean;
+        showPicture?: boolean;
+        showEditButton?: boolean;
+        showStatsButton?: boolean;
+        showExitButton?: boolean;
+        showBalance?: boolean;
+        showPaymentButton?: boolean;
+      };
+      showHome?: boolean;
+      showCash?: boolean;
+      showOrders?: boolean;
+      showInvoices?: boolean;
+      showBaskets?: boolean;
+      showCustomers?: boolean;
+      showProducts?: boolean;
+      showReports?: boolean;
+      store?: {
+        show?: boolean;
+        showConfig?: boolean;
+        showCategories?: boolean;
+        showBrands?: boolean;
+        showProviders?: boolean;
+        showUsers?: boolean;
+        showIntegrations?: boolean;
+      };
+      showBalance?: boolean;
+    };
+    footer?: {
+      desktopNavigation?: {
+        show?: boolean;
+        showCopyright?: boolean;
+        showPrivacy?: boolean;
+        showAppVersion?: boolean;
+      };
+      mobileNavigation?: {
+        show?: boolean;
+        showOrders?: boolean;
+        showInvoices?: boolean;
+        showBasket?: boolean;
+        showFavorites?: boolean;
+      };
+    };
+  }
 
-	interface Store {
-		storeId: string;
-		createdAt: number;
-		name: string;
-		address: {
-			street: string;
-			postalCode: string;
-			city: string;
-			province: string;
-		};
-		cuit: string;
-		phone: number;
-		email: string;
-		// CONFIG
-		config: {
-			priceDecimals: 0 | 1 | 2 | 3;
-			stock: boolean;
-			changePrice: boolean;
-			currency: number;
-		};
-		// ECOMMERCE
-		ecommerce?: Ecommerce;
-		// HANDLE IMAGES
-		photoURL: string;
-		photoData?: string;
-		newPhotoURL?: string;
-		removePhotoURL?: string;
-		// GENERAL
-		currencies: Method[];
-		cashInMethods: Method[];
-		cashOutMethods: Method[];
-		debitMethods: Method[];
-		// CUSTOMERS
-		priceLists: Method[];
-		accountMethods: Method[];
-		deliveryMethods: Method[];
-		paymentMethods: Method[];
-		// PRODUCTS
-		brands: Brand[];
-		categories: Category[];
-		// COLORS
-		themeColors?: {
-			main?: string;
-			navbar?: string;
-		};
-		stats: {
-			customers?: number;
-			invoices?: number;
-			orders?: number;
-			products?: number;
-			users?: number;
-		};
-		mercadopago?: {
-			accessToken?: string;
-			code?: string;
-			refreshToken?: string;
-		};
-		// AFIP
-		afip: Afip;
-		// FROM CONFIG PK
-		appVersion: number;
-		fiscalConditions: FiscalCondition[];
-		ivaTypes: Method[];
-		minWithDni: number;
-		notificationOptions?: Method[];
-	}
+  interface Store {
+    storeId: string;
+    createdAt: number;
+    name: string;
+    address: {
+      street: string;
+      postalCode: string;
+      city: string;
+      province: string;
+    };
+    cuit: string;
+    phone: number;
+    email: string;
+    // CONFIG // NOT FEATURE FLAGS // FUNCTIONAL CONFIG
+    config: {
+      priceDecimals: 0 | 1 | 2 | 3;
+      stock: boolean;
+      changePrice: boolean;
+      currency: number;
+    };
+    features: FeatureFlags;
+    // ECOMMERCE
+    ecommerce?: Ecommerce;
+    // HANDLE IMAGES
+    photoURL: string;
+    photoData?: string;
+    newPhotoURL?: string;
+    removePhotoURL?: string;
+    // GENERAL
+    currencies: Method[];
+    cashInMethods: Method[];
+    cashOutMethods: Method[];
+    debitMethods: Method[];
+    // CUSTOMERS
+    priceLists: Method[];
+    accountMethods: Method[];
+    deliveryMethods: Method[];
+    paymentMethods: Method[];
+    // PRODUCTS
+    brands: Brand[];
+    categories: Category[];
+    // COLORS
+    themeColors?: {
+      main?: string;
+      navbar?: string;
+    };
+    stats: {
+      customers?: number;
+      invoices?: number;
+      orders?: number;
+      products?: number;
+      users?: number;
+    };
+    mercadopago?: {
+      accessToken?: string;
+      code?: string;
+      refreshToken?: string;
+    };
+    // AFIP
+    afip: Afip;
+    // FROM CONFIG PK
+    appVersion: number;
+    fiscalConditions: FiscalCondition[];
+    ivaTypes: Method[];
+    minWithDni: number;
+    notificationOptions?: Method[];
+  }
 
-	interface Afip {
-		production: boolean;
-		// ADDRESS
-		address?: string;
-		city?: string;
-		condFiscal?: number;
-		cuit?: string;
-		condFiscalName?: string;
-		postalCode?: string;
-		province?: string;
-		razonSocial?: string;
-		// NEW
-		pointOfSale?: number; // PTO_VTA
-		activitiesStartedAt?: number; // INICIO_ACTIVIDADES
-		invoiceNote?: string; // NOTA EN FACTURA
-		showInvoiceLogo?: string; // logo en factura
-		currency: 1 | 2; // 1 PESOS, 2 DOLARES
-		// ACCESS
-		cert?: string;
-		csr?: string;
-		key?: string;
-		accessTicket_EB?: string;
-		accessTicket_RSF?: string;
-	}
+  interface Afip {
+    production: boolean;
+    // ADDRESS
+    address?: string;
+    city?: string;
+    condFiscal?: number;
+    cuit?: string;
+    condFiscalName?: string;
+    postalCode?: string;
+    province?: string;
+    razonSocial?: string;
+    // NEW
+    pointOfSale?: number; // PTO_VTA
+    activitiesStartedAt?: number; // INICIO_ACTIVIDADES
+    invoiceNote?: string; // NOTA EN FACTURA
+    showInvoiceLogo?: string; // logo en factura
+    currency: 1 | 2; // 1 PESOS, 2 DOLARES
+    // ACCESS
+    cert?: string;
+    csr?: string;
+    key?: string;
+    accessTicket_EB?: string;
+    accessTicket_RSF?: string;
+  }
 
-	type StoreAttributeNames = keyof Store;
+  type StoreAttributeNames = keyof Store;
 
-	interface Method {
-		id: number;
-		name: string;
-		value?: number;
-		removable?: boolean;
-		editable?: boolean;
-	}
+  interface Method {
+    id: number;
+    name: string;
+    value?: number;
+    removable?: boolean;
+    editable?: boolean;
+  }
 
-	interface Category {
-		categoryId: string;
-		name: string;
-		photoURL?: string;
-		photoData?: string;
-		removePhotoURL?: string;
-		isFather?: boolean;
-		father?: number;
-		disabled?: boolean;
-		isNew?: boolean;
-	}
-	interface Brand {
-		brandId: string;
-		name: string;
-		photoURL?: string;
-		photoData?: string;
-		removePhotoURL?: string;
-		isFather?: boolean;
-		father?: number;
-		disabled?: boolean;
-		isNew?: boolean;
-	}
+  interface Category {
+    categoryId: string;
+    name: string;
+    photoURL?: string;
+    photoData?: string;
+    removePhotoURL?: string;
+    isFather?: boolean;
+    father?: number;
+    disabled?: boolean;
+    isNew?: boolean;
+  }
+  interface Brand {
+    brandId: string;
+    name: string;
+    photoURL?: string;
+    photoData?: string;
+    removePhotoURL?: string;
+    isFather?: boolean;
+    father?: number;
+    disabled?: boolean;
+    isNew?: boolean;
+  }
 
-	interface FiscalCondition {
-		CbteTipo: {
-			FAC: number;
-			NC: number,
-			ND: number;
-			NVC: number;
-			REC: number;
-		};
-		DocTipo: number;
-		condFiscal: number;
-		id: number;
-		name: string;
-	}
-
+  interface FiscalCondition {
+    CbteTipo: {
+      FAC: number;
+      NC: number;
+      ND: number;
+      NVC: number;
+      REC: number;
+    };
+    DocTipo: number;
+    condFiscal: number;
+    id: number;
+    name: string;
+  }
 }
 
-export { };
+export {};
