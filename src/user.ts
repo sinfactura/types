6015060e247a6fc3586c0dbcc28954097faadd34
Moises Samuel Maison <toinmdq@gmail.com>
2026-03-28 00:48:35 +0100

    chore: update .gitignore and refactor User interface for improved readability

--- src/user.ts
+++ src/user.ts
@@ -1,57 +1,54 @@
-
 declare global {
+  interface User {
+    storeId: string;
+    userId: string;
+    createdAt: number;
+    fullName: string;
+    phone: string;
+    email: string;
+    password?: string;
+    roles: string;
+    photoURL: string;
+    disabled: boolean;
+    search: string;
+    accessToken: string;
+    // ROLES
+    roleSeller?: boolean;
+    roleProducts?: boolean;
+    roleCustomers?: boolean;
+    roleAfip?: boolean;
+    // NOTIFICATIONS2
+    notifications?: UserNotifications;
+    permissions?: UserPermissions;
+  }
 
-	interface User {
-		storeId: string;
-		userId: string;
-		createdAt: number;
-		fullName: string;
-		phone: string;
-		email: string;
-		password?: string;
-		roles: string;
-		photoURL: string;
-		disabled: boolean;
-		search: string;
-		accessToken: string;
-		// ROLES
-		roleSeller?: boolean;
-		roleProducts?: boolean;
-		roleCustomers?: boolean;
-		roleAfip?: boolean;
-		// NOTIFICATIONS
-		notifications?: UserNotifications;
-		permissions?: UserPermissions;
-	}
+  type UserNotifications = {
+    orders?: boolean;
+    mercadopago?: boolean;
+    dollarOficial?: boolean;
+    dollarInformal?: boolean;
+    dollarBna?: boolean;
+    printer?: boolean;
+  };
 
-	type UserNotifications = {
-		orders?: boolean;
-		mercadopago?: boolean;
-		dollarOficial?: boolean;
-		dollarInformal?: boolean;
-		dollarBna?: boolean;
-		printer?: boolean;
-	}
+  type UserPermissions = {
+    currency?: boolean;
+    customers?: boolean;
+    products?: boolean;
+    seller?: boolean;
+    accountant?: boolean;
+    payments?: boolean;
+    cash?: boolean;
+    packOrder?: boolean;
+  };
 
-	type UserPermissions = {
-		currency?: boolean;
-		customers?: boolean;
-		products?: boolean;
-		seller?: boolean;
-		accountant?: boolean;
-		payments?: boolean;
-		cash?: boolean;
-		packOrder?: boolean;
-	}
+  interface UserGoogle extends User {
+    displayName: string;
+  }
 
-	interface UserGoogle extends User {
-		displayName: string;
-	}
-
-	interface AuthUser extends User {
-		refreshToken: string;
-	}
-
+  interface AuthUser extends User {
+    refreshToken: string;
+  }
 }
 
-export { };
+export {};
