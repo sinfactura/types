1cbd95065588ed1802fabb54a0f76d651a8f2793
Moises Samuel Maison <toinmdq@gmail.com>
2026-05-28 22:29:21 +0200

    refactor(stock): clean up interface formatting and maintain consistency

--- src/stock.ts
+++ src/stock.ts
@@ -1,28 +1,25 @@
-
 declare global {
+  interface StockBase {
+    storeId: string;
+    stockId: string; // income-PROD000330
+    createdAt: number; // add when insert an item
+    cost: number; // 1.23
+    skip?: boolean; // new to replace old notEvaluate // unnecesary in all new inserts
+  }
 
-	interface StockBase {
-		storeId: string;
-		stockId: string; // income-PROD000330
-		createdAt: number; // add when insert an item
-		cost: number; // 1.23
-		skip?: boolean // new to replace old notEvaluate // unnecesary in all new inserts
-	}
+  interface StockIncome extends StockBase {
+    quantity: number; // income
+    supplierId?: string; // income
+    supplierName?: string; // income
+  }
 
-	interface StockIncome extends StockBase {
-		quantity: number; // income
-		supplierId?: number; // income
-		supplierName?: string; // income
-	}
-
-	interface StockSale extends StockBase {
-		customerId?: string; // sale
-		fullName?: string; // sale
-		ivaType?: number; // sale
-		orderId?: string; // sale
-		price?: number; // sale
-	}
-
+  interface StockSale extends StockBase {
+    customerId?: string; // sale
+    fullName?: string; // sale
+    ivaType?: number; // sale
+    orderId?: string; // sale
+    price?: number; // sale
+  }
 }
 
 export {}; // NOSONAR
